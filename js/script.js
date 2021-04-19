01c7b84b05f010dbcbbc38a4527eccea441ba6a8
Caterina Bertotti <bertotti4997@gmail.com>
2021-04-19 14:00:52 +0200

    Header: voci nav dinamiche
    Aggiunto voci dinamiche, finire effetto hover

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var app = new Vue (
+    {
+        el: '#root',
+        data: {
+            pages: [
+                'home',
+                'pages',
+                'portfolio',
+                'blog',
+                'shop',
+                'elements'
+            ]
+        },
+        methods: {
+
+        }
+    }
+);
