cc08f79b04e016d227c945b9a59c08b727f5cdb0
Caterina Bertotti <bertotti4997@gmail.com>
2021-04-21 18:13:08 +0200

    Contact UI

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 // webpack.mix.js
 let mix = require('laravel-mix');
 
-mix.sass('src/style.scss', 'dist').options({
-    processCssUrls: false
-});
+mix.sass('src/app.scss', 'dist');
+
+mix.options({ processCssUrls: false });
